eabf47ef8ca1b374420f5cc714d0f20800bd8b1e
Elliott Greaves <greaveselliott@me.com>
2019-06-20 23:45:05 +0100

    Added some styling

--- reduxless/src/App.js
+++ reduxless/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useContext } from "react";
 
 import { Store } from "./Store";
 import { fetchDataAction } from "./Actions";
-import List from "./List";
+import "./app.scss";
 
 export default function App() {
   const { state, dispatch } = useContext(Store);
@@ -11,5 +11,25 @@ export default function App() {
     state.todos.length === 0 && fetchDataAction(dispatch);
   });
 
-  return <List listData={state.todos} />;
+  return (
+    <main className="app">
+      <h1 className="app__title">To do list.</h1>
+      <form className="app__form">
+        <input type="text" className="app__input" />
+        <input
+          type="submit"
+          className="app__input-submit"
+          value="Add to list"
+        />
+      </form>
+      <ul className="app__list">
+        {state.todos.map(listItem => (
+          <li className="app__list-item" key={listItem.id}>
+            {listItem.title}
+            <button className="app__list-item-delete">Delete</button>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
