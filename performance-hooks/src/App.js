dae06de71303208366eeed6bb954871cf76d613b
Elliott Greaves <greaveselliott@me.com>
2019-06-30 22:57:50 +0100

    removing uuid from window

--- performance-hooks/src/App.js
+++ performance-hooks/src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useContext } from "react";
 
-function App() {
+import { Store } from "./Store";
+import { createNewListItem, deleteNewListItem } from "./Actions";
+import "./app.scss";
+
+export default function App() {
+  const { state, dispatch } = useContext(Store);
+
+  const input = useRef(null);
+  const onFormSubmit = event => {
+    event.preventDefault();
+
+    const listItemValue = input.current.value;
+
+    if (listItemValue !== "") {
+      createNewListItem({
+        dispatch,
+        payload: { listItemValue }
+      });
+
+      input.current.value = "";
+    }
+  };
+
+  const onDeleteListItem = uuid => {
+    deleteNewListItem({
+      dispatch,
+      payload: {
+        uuid
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="app">
+      <h1 className="app__title">To do list</h1>
+      <form className="app__form" onSubmit={onFormSubmit}>
+        <input type="text" className="app__input" ref={input} />
+        <input
+          type="submit"
+          className="app__input-submit"
+          value="Add to list"
+        />
+      </form>
+      <ul className="app__list">
+        {state.list.map((listItem, index) => (
+          <li className="app__list-item" key={`list-item-${index}`}>
+            {listItem.listItemValue}
+            <button
+              className="app__list-item-delete"
+              onClick={() => onDeleteListItem(listItem.uuid)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
-
-export default App;
