5fdf3ed447380c0ea37c65f49f49d02bf3230ff8
Elliott Greaves <greaveselliott@me.com>
2019-07-14 02:33:36 +0100

    Created fake API for retrieving images

--- performance-hooks/src/App.js
+++ performance-hooks/src/App.js
@@ -1,61 +1,17 @@
 import React, { useRef, useContext } from "react";
-
-import { Store } from "./Store";
-import { createNewListItem, deleteNewListItem } from "./Actions";
 import "./app.scss";
 
 export default function App() {
-  const { state, dispatch } = useContext(Store);
-
-  const input = useRef(null);
-  const onFormSubmit = event => {
-    event.preventDefault();
-
-    const listItemValue = input.current.value;
-
-    if (listItemValue !== "") {
-      createNewListItem({
-        dispatch,
-        payload: { listItemValue }
-      });
-
-      input.current.value = "";
-    }
-  };
-
-  const onDeleteListItem = uuid => {
-    deleteNewListItem({
-      dispatch,
-      payload: {
-        uuid
-      }
-    });
-  };
-
   return (
     <main className="app">
-      <h1 className="app__title">To do list</h1>
-      <form className="app__form" onSubmit={onFormSubmit}>
-        <input type="text" className="app__input" ref={input} />
-        <input
-          type="submit"
-          className="app__input-submit"
-          value="Add to list"
-        />
-      </form>
-      <ul className="app__list">
-        {state.list.map((listItem, index) => (
-          <li className="app__list-item" key={`list-item-${index}`}>
-            {listItem.listItemValue}
-            <button
-              className="app__list-item-delete"
-              onClick={() => onDeleteListItem(listItem.uuid)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+      <header>
+        <h1 className="app__title">Flyckr</h1>
+        <form>
+          <input type="text" />
+          <input type="submit" />
+        </form>
+      </header>
+      <section />
     </main>
   );
 }
--- performance-hooks/src/index.js
+++ performance-hooks/src/index.js
@@ -2,11 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-import { StoreProvider } from "./Store";
-
-ReactDOM.render(
-  <StoreProvider>
-    <App />
-  </StoreProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
