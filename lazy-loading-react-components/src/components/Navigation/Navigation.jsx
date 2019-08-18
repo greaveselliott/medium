6939c50581b70711f2efbfcd736b700d5302389a
Elliott Greaves <greaveselliott@me.com>
2019-08-19 00:25:29 +0100

    some initial styles

--- lazy-loading-react-components/src/components/Navigation/Navigation.jsx
+++ lazy-loading-react-components/src/components/Navigation/Navigation.jsx
@@ -1,45 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./navigation.scss";
 
-const Navigation = () => (
-  <nav className="navigation">
+const navigationConfig = [
+  { path: "/", label: "Home" },
+  { path: "/about", label: "About" },
+  { path: "/gallery", label: "Gallery" },
+  { path: "/contact", label: "Contact" }
+];
+
+const Navigation = ({ config = navigationConfig }) => (
+  <nav className="navigation" role="menu">
     <ul className="navigation__list">
-      <li className="navigation__list-item">
-        <NavLink
-          to="/"
-          className="navigation__link"
-          activeClassName="navigation__link--active"
-        >
-          Home
-        </NavLink>
-      </li>
-      <li className="navigation_list-item">
-        <NavLink
-          to="/about"
-          className="navigation__link"
-          activeClassName="navigation__link--active"
-        >
-          About
-        </NavLink>
-      </li>
-      <li className="navigation_list-item">
-        <NavLink
-          to="/gallery"
-          className="navigation__link"
-          activeClassName="navigation__link--active"
-        >
-          Gallery
-        </NavLink>
-      </li>
-      <li className="navigation_list-item">
-        <NavLink
-          to="/contact"
-          className="navigation__link"
-          activeClassName="navigation__link--active"
-        >
-          Contact
-        </NavLink>
-      </li>
+      {config.map(({ path, label }) => (
+        <li className="navigation__list-item">
+          <NavLink
+            to={path}
+            exact
+            className="navigation__link"
+            activeClassName="navigation__link--active"
+          >
+            {label}
+          </NavLink>
+        </li>
+      ))}
     </ul>
   </nav>
 );
